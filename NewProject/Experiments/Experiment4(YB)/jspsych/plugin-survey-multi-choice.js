d5e14e994322a03515ba9d7c09454a0e8fd66983
CongleiXu <109285326+CongleiXu@users.noreply.github.com>
2024-06-11 17:24:34 +0200

    Update plugin-survey-multi-choice.js

--- NewProject/Experiments/Experiment4(YB)/jspsych/plugin-survey-multi-choice.js
+++ NewProject/Experiments/Experiment4(YB)/jspsych/plugin-survey-multi-choice.js
@@ -138,7 +138,7 @@ var jsPsychSurveyMultiChoice = (function (jspsych) {
               // add question text
               html += '<p class="jspsych-survey-multi-choice-text survey-multi-choice">' + question.prompt;
               if (question.required) {
-                  html += "<span class='required'>*</span>";
+                  html += "<span class='required'></span>";
               }
               html += "</p>";
               // create option radio buttons
