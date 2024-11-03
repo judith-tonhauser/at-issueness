eef170564157560977beced3bf81994ffc1f0a90
CongleiXu <109285326+CongleiXu@users.noreply.github.com>
2024-11-03 17:39:55 +0100

    Add files via upload

---
+++ CongleiThesis/results/main/comparisons/rscripts/analysis(qud:yb).R
@@ -0,0 +1,76 @@
+#YB VS. QUD
+this.dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(this.dir)
+
+source('helpers.R')
+
+# load required packages
+library(tidyverse)
+library(dplyr)
+library(dichromat)
+library(forcats)
+library(ggrepel)
+library(cowplot)
+theme_set(theme_bw())
+
+# load clean data for analysis ----
+
+# load clean projectivity data for analysis
+yb = read.csv("../../exp4/data/cd.csv")
+qud = read.csv("../../exp1/data/cd.csv") 
+
+# projection: binary vs continuous ----
+
+# for projectivity data, plot proportions against mean slider ratings
+yb_means = yb %>%
+  group_by(expression) %>%
+  summarize(Mean_yb = mean(nResponse), CILow_yb = ci.low(nResponse), CIHigh_yb = ci.high(nResponse)) %>%
+  mutate(YMinM = Mean_yb - CILow_yb, YMaxM = Mean_yb + CIHigh_yb, expression = fct_reorder(as.factor(expression), Mean_yb)) %>%
+  select(-CILow_yb, -CIHigh_yb)
+
+qud_means = qud %>%
+  group_by(expression) %>%
+  summarize(Mean_qud = mean(response), CILow_qud = ci.low(response), CIHigh_qud = ci.high(response)) %>%
+  mutate(YMinP = Mean_qud - CILow_qud, YMaxP = Mean_qud + CIHigh_qud, expression = fct_reorder(as.factor(expression), Mean_qud)) %>%
+  select(-CILow_qud, -CIHigh_qud)
+
+qud_yb = qud_means %>%
+  left_join(yb_means, by = "expression")
+
+# Define a vector of shapes for 7 expressions (using valid shape values)
+shapes <- c(21, 22, 23, 24, 25, 0, 1)
+names(shapes) <- levels(qud_yb$expression)
+
+# Define a vector of colors for 7 expressions
+colors <- c("darkorchid", "black", "gray60", "tomato1", "dodgerblue", "forestgreen", "gold")
+names(colors) <- levels(qud_yb$expression)
+
+# shape-predicate mapping
+# 21: mc
+# 22: non-veridical non-factive NF
+# 23: factive F
+# 24: optionally factive V
+# 25: veridical non-factive VNF
+# 0, 1: Add appropriate labels if needed
+
+pp <- ggplot(qud_yb, aes(x = Mean_qud, y = Mean_yb, fill = expression, shape = expression)) +
+  geom_errorbar(aes(ymin = YMinM, ymax = YMaxM), width = 0) +
+  geom_errorbar(aes(xmin = YMinP, xmax = YMaxP), width = 0) +
+  geom_point(stroke = .5, size = 2.5, color = "black") +
+  geom_abline(intercept = 0, slope = 1, color = "gray70", linetype = "dashed") +
+  geom_text_repel(aes(label = expression), size = 3, max.overlaps = 10) +
+  ylab("Proportion of 'no' (at-issueness) selections ('yes, but' test)") +
+  xlab("Mean QUD (at-issueness) ratings") +
+  theme(legend.position = "none") +
+  coord_fixed(ratio = 1) +
+  scale_x_continuous(limits = c(0, 1.05), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0, 1.05), expand = c(0, 0)) +
+  scale_shape_manual(values = shapes) +
+  scale_fill_manual(values = colors)
+
+pp
+ggsave("../graphs/YBvsQUD.pdf", height = 5, width = 5)
+
+
+
+
